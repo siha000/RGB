364396b57f759c13a7941bd77c3afc84deb1982e
siha <applegreen7841@gmail.com>
2022-09-06 01:53:00 +0800

    1

--- LOW/LOW.c
+++ LOW/LOW.c
@@ -2,96 +2,36 @@
 #include <stdlib.h>
 #define HEIGHT 512
 #define WEIGHT 512
-unsigned char record[HEIGHT][WEIGHT] = {0};
-unsigned char R[HEIGHT][WEIGHT] = {0};
-unsigned char G[HEIGHT][WEIGHT] = {0};
-unsigned char B[HEIGHT][WEIGHT] = {0};
 unsigned char RGB[HEIGHT][WEIGHT * 3] = {0};
-unsigned char DP_R[HEIGHT][WEIGHT] = {0};
-unsigned char DP_G[HEIGHT][WEIGHT] = {0};
-unsigned char DP_B[HEIGHT][WEIGHT] = {0};
-unsigned char result[HEIGHT][WEIGHT * 3] = {0};
+unsigned char record[HEIGHT + 2][WEIGHT * 3 + 6] = {0};
+unsigned char result[HEIGHT][WEIGHT * 3];
 int main()
 {
     FILE *read = fopen("TEXT.rgb", "rb");
     FILE *write = fopen("TEXT1.rgb", "wb");
     fread(RGB, sizeof(unsigned char), HEIGHT * WEIGHT * 3, read);
-    for (int c = 0; c < HEIGHT; c++)
+    for (int c = 1; c < HEIGHT; c++)
     {
-        for (int d = 0; d < WEIGHT; d++)
+        for (int d = 3; d < WEIGHT * 3; d++)
         {
-            R[c][d] = RGB[c][d * 3];
-            G[c][d] = RGB[c][d * 3 + 1];
-            B[c][d] = RGB[c][d * 3 + 2];
-        }
-    }
-
-    for (int c = 0; c < HEIGHT; c++)
-    {
-        for (int d = 0; d < WEIGHT; d++)
-        {
-            /*if (c + 2 < HEIGHT)
-            {
-                DP_R[c][d] = R[c][d] + R[c + 1][d] + R[c + 2][d];
-                DP_G[c][d] = G[c][d] + G[c + 1][d] + G[c + 2][d];
-                DP_B[c][d] = B[c][d] + B[c + 1][d] + B[c + 2][d];
-            }
-            else
-            {
-                if (c + 1 == HEIGHT)
-                {
-                    DP_R[c][d] = R[c][d];
-                    DP_G[c][d] = G[c][d];
-                    DP_B[c][d] = B[c][d];
-                }
-                else
-                {
-                    DP_R[c][d] = R[c][d] + R[c + 1][d];
-                    DP_G[c][d] = G[c][d] + G[c + 1][d];
-                    DP_B[c][d] = B[c][d] + B[c + 1][d];
-                }
-            }
-            */
-            DP_R[c][d] = R[c][d] + R[c + 1][d] + R[c + 2][d];
-            DP_G[c][d] = G[c][d] + G[c + 1][d] + G[c + 2][d];
-            DP_B[c][d] = B[c][d] + B[c + 1][d] + B[c + 2][d];
+            record[c][d] = RGB[c - 1][d - 3];
         }
     }
     for (int f = 0; f < 10; f++)
     {
-        for (int c = 0; c < HEIGHT; c++)
+        for (int c = 1; c < HEIGHT; c++)
         {
-            for (int d = 0; d < WEIGHT; d++)
+            for (int d = 3; d < WEIGHT * 3; d += 3)
             {
-                char record_R, record_G, record_B;
-                /*if (d + 2 < WEIGHT)
-                {
-                    record_R = (DP_R[c][d] + DP_R[c][d + 1] + DP_R[c][d + 2]) / 9;
-                    record_G = (DP_G[c][d] + DP_G[c][d + 1] + DP_G[c][d + 2]) / 9;
-                    record_B = (DP_B[c][d] + DP_B[c][d + 1] + DP_B[c][d + 2]) / 9;
-                }
-                else
-                {
-                    if (d + 1 == WEIGHT)
-                    {
-                        record_R = DP_R[c][d] / 9;
-                        record_G = DP_G[c][d] / 9;
-                        record_B = DP_B[c][d] / 9;
-                    }
-                    else
-                    {
-                        record_R = (DP_R[c][d] + DP_R[c][d + 1]) / 9;
-                        record_G = (DP_G[c][d] + DP_G[c][d + 1]) / 9;
-                        record_B = (DP_B[c][d] + DP_B[c][d + 1]) / 9;
-                    }
-                }
-                */
-                record_R = (R[c][d] + R[c][d + 1] + R[c][d + 2]) / 3;
-                record_G = (G[c][d] + G[c][d + 1] + G[c][d + 2]) / 3;
-                record_B = (B[c][d] + B[c][d + 1] + B[c][d + 2]) / 3;
-                result[c][d * 3] = record_R;
-                result[c][d * 3 + 1] = record_G;
-                result[c][d * 3 + 2] = record_B;
+                char record_R = (record[c][d] + record[c][d + 3] + record[c][d - 3] + record[c - 1][d] + record[c - 1][d + 3] + record[c - 1][d - 3] + record[c + 1][d] + record[c + 1][d + 3] + record[c + 1][d - 3]) / 9;
+                char record_G = (record[c][d + 1] + record[c][d + 4] + record[c][d - 2] + record[c - 1][d + 1] + record[c - 1][d + 4] + record[c - 1][d - 2] + record[c + 1][d + 1] + record[c + 1][d + 4] + record[c + 1][d - 2]) / 9;
+                char record_B = (record[c][d + 2] + record[c][d + 5] + record[c][d - 1] + record[c - 1][d + 2] + record[c - 1][d + 5] + record[c - 1][d - 1] + record[c + 1][d + 2] + record[c + 1][d + 5] + record[c + 1][d - 1]) / 9;
+                result[c - 1][d - 3] = record_R;
+                result[c - 1][d - 2] = record_G;
+                result[c - 1][d - 1] = record_B;
+                record[c][d] = record_R;
+                record[c][d + 1] = record_G;
+                record[c][d + 2] = record_B;
             }
         }
     }
--- medium/medium.c
+++ medium/medium.c
@@ -3,9 +3,7 @@
 #define HEIGHT 512
 #define WEIGHT 512
 unsigned char RGB[HEIGHT][WEIGHT * 3];
-char R[HEIGHT][WEIGHT];
-char G[HEIGHT][WEIGHT];
-char B[HEIGHT][WEIGHT];
+char record[HEIGHT + 2][WEIGHT * 3 + 6] = {0};
 unsigned char result[HEIGHT][WEIGHT * 3];
 char bubble_sort(char *data)
 {
@@ -29,62 +27,56 @@ int main()
     FILE *read = fopen("TEXT.rgb", "rb");
     FILE *write = fopen("TEXT1.rgb", "wb");
     fread(RGB, sizeof(unsigned char), HEIGHT * WEIGHT * 3, read);
-    for (int c = 0; c < HEIGHT; c++)
+    for (int c = 1; c < HEIGHT; c++)
     {
-        for (int d = 0; d < WEIGHT; d++)
+        for (int d = 3; d < WEIGHT * 3; d++)
         {
-            R[c][d] = RGB[c][d * 3];
-            G[c][d] = RGB[c][d * 3 + 1];
-            B[c][d] = RGB[c][d * 3 + 2];
+            record[c][d] = RGB[c - 1][d - 3];
         }
     }
-
     for (int c = 0; c < HEIGHT; c++)
     {
-        for (int d = 0; d < WEIGHT; d++)
+        for (int d = 0; d < WEIGHT * 3; d += 3)
         {
-            if (c + 2 < HEIGHT && d + 2 < WEIGHT)
-            {
-                char data_R[9] = {R[c][d],
-                                  R[d][d + 1],
-                                  R[c][d + 2],
-                                  R[c + 1][d],
-                                  R[c + 1][d + 1],
-                                  R[c + 1][d + 2],
-                                  R[c + 2][d],
-                                  R[c + 2][d + 1],
-                                  R[c + 2][d + 2]};
+            char data_R[9] = {record[c][d],
+                              record[c][d + 3],
+                              record[c][d - 3],
+                              record[c - 1][d],
+                              record[c - 1][d + 3],
+                              record[c - 1][d - 3],
+                              record[c + 1][d],
+                              record[c + 1][d + 3],
+                              record[c + 1][d - 3]};
 
-                char data_G[9] = {G[c][d],
-                                  G[d][d + 1],
-                                  G[c][d + 2],
-                                  G[c + 1][d],
-                                  G[c + 1][d + 1],
-                                  G[c + 1][d + 2],
-                                  G[c + 2][d],
-                                  G[c + 2][d + 1],
-                                  G[c + 2][d + 2]};
+            char data_G[9] = {record[c][d + 1],
+                              record[c][d + 4],
+                              record[c][d - 2],
+                              record[c - 1][d + 1],
+                              record[c - 1][d + 4],
+                              record[c - 1][d - 2],
+                              record[c + 1][d + 1],
+                              record[c + 1][d + 4],
+                              record[c + 1][d - 2]};
 
-                char data_B[9] = {B[c][d],
-                                  B[d][d + 1],
-                                  B[c][d + 2],
-                                  B[c + 1][d],
-                                  B[c + 1][d + 1],
-                                  B[c + 1][d + 2],
-                                  B[c + 2][d],
-                                  B[c + 2][d + 1],
-                                  B[c + 2][d + 2]};
+            char data_B[9] = {record[c][d + 2],
+                              record[c][d + 5],
+                              record[c][d - 1],
+                              record[c - 1][d + 2],
+                              record[c - 1][d + 5],
+                              record[c - 1][d - 1],
+                              record[c + 1][d + 2],
+                              record[c + 1][d + 5],
+                              record[c + 1][d - 1]};
 
-                char record_R, record_G, record_B;
+            char record_R, record_G, record_B;
 
-                record_R = bubble_sort(data_R);
-                record_G = bubble_sort(data_G);
-                record_B = bubble_sort(data_B);
+            record_R = bubble_sort(data_R);
+            record_G = bubble_sort(data_G);
+            record_B = bubble_sort(data_B);
 
-                result[c][d * 3] = record_R;
-                result[c][d * 3 + 1] = record_G;
-                result[c][d * 3 + 2] = record_B;
-            }
+            result[c - 1][d - 3] = record_R;
+            result[c - 1][d - 2] = record_G;
+            result[c - 1][d - 1] = record_B;
         }
     }
 
