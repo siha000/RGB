34d2f8a2bfabf3d0e3434f4624d6dd75d9c76d52
siha000 <applegreen7841@gmail.com>
2022-09-14 22:29:37 +0800

    1

--- gray/gray.c
+++ gray/gray.c
@@ -2,24 +2,28 @@
 #include <stdlib.h>
 #define HEIGHT 512
 #define WEIGHT 512
-unsigned char RGB[HEIGHT][WEIGHT * 3];
+/*unsigned char RGB[HEIGHT][WEIGHT * 3];
 unsigned char record[HEIGHT][WEIGHT * 3] = {0};
+*/
 int main()
 {
     FILE *read = fopen("TEXT.rgb", "rb");
     FILE *write = fopen("TEXT1.rgb", "wb");
+    unsigned char *RGB = (unsigned char *)malloc(sizeof(unsigned char) * HEIGHT * WEIGHT * 3);
+    unsigned char *record = (unsigned char *)malloc(sizeof(unsigned char) * HEIGHT * WEIGHT * 3);
+
     fread(RGB, sizeof(unsigned char), HEIGHT * WEIGHT * 3, read);
-    for (int c = 0; c < HEIGHT; c++)
+    for (int c = 0; c < HEIGHT * WEIGHT * 3; c += 3)
     {
-        for (int d = 0; d < WEIGHT * 3; d+=3)
-        {
-            char temp = (RGB[c][d] + RGB[c][d + 1] + RGB[c][d + 2]) / 3;
-            record[c][d] = temp;
-            record[c][d + 1] = temp;
-            record[c][d + 2] = temp;
-        }
+
+        char temp = (RGB[c] + RGB[c + 1] + RGB[c + 2]) / 3;
+        record[c] = temp;
+        record[c + 1] = temp;
+        record[c + 2] = temp;
     }
     fwrite(record, sizeof(unsigned char), HEIGHT * WEIGHT * 3, write);
     fclose(read);
     fclose(write);
+    free(RGB);
+    free(record);
 }
